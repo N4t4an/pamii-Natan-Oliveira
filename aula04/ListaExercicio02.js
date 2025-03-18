9708c631ab40025a4d715e555763a26feb21c76c
N4t4an <incrivelnatan@gmail.com>
2025-03-18 17:49:55 -0300

    Exercicios da nayra

---
+++ aula04/ListaExercicio02.js
@@ -0,0 +1,4 @@
+Numero = parseInt(prompt("Digite um Numero"))
+if(Numero>0){
+    window.alert("Seu numero é negativo")
+}
---
+++ aula04/ListaExercicios01.js
@@ -0,0 +1,8 @@
+Numero = parseInt(prompt("Digite um numero"))
+
+if(Numero%2 == 0){
+    window.alert("Seu numero é par")
+}
+else {
+    window.alert("Seu numero é impar")
+}
